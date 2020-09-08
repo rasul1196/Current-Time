9312894cf565bf2732d8177d42cb6fae42d56cc0
Rasul Sherimmatov <64540119+rasul1196@users.noreply.github.com>
2020-09-08 16:27:31 +0900

    Add files via upload

---
+++ sana.js
@@ -0,0 +1,7 @@
+function sana() {
+var time = new Date();
+var hours = time.getHours();
+var minutes = time.getMinutes();
+var seconds = time.getSeconds();
+document.write(hours+":",minutes+":",seconds);
+}
